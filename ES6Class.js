d6cba60711d66259ad0a04b4193707cc5e618650
Shreyas <pshreyas2827@gmail.com>
2023-06-14 13:38:54 +0530

    Added new code

---
+++ ES6Class.js
@@ -0,0 +1,26 @@
+class API{
+    constructor(url, method='GET'){
+        this.url = url;
+        this.method = method
+    }
+    #secure = false
+    isSecureContext(){
+        // if(this.url.split("://")[0] === "http"){
+        //     return false;                             //another way
+        // }else{
+        //     return true;
+        // }
+        return this.url.split("://")[0] === "https";
+    }
+    updateUrl(newURL){
+        this.url = newURL;
+        this.#secure = this.isSecure()
+    }
+}
+const s = new API("https://api.com/api/hello");
+console.log(s.isSecure());
+
+s.updateUrl("https://api.com/api/hello");
+console.log(s.isSecure());
+
+console.log(s.url);
