afcac95a29a53967a86f0b7479889fdb4d7c7ab8
Shreyas Patil <110882047+shreefsd@users.noreply.github.com>
2023-06-15 14:39:06 +0530

    Update ES6Class.js
    New code

--- ES6Class.js
+++ ES6Class.js
@@ -1,27 +1,21 @@
-class API{
-    constructor(url, method='GET'){
-        this.url = url;
-        this.method = method
-    }
-    #secure = false
-    isSecureContext(){
-        // if(this.url.split("://")[0] === "http"){
-        //     return false;                             //another way
-        // }else{
-        //     return true;
-        // }
-        return this.url.split("://")[0] === "https";
-    }
-    updateUrl(newURL){
-        this.url = newURL;
-        this.#secure = this.isSecure()
-    }
+class API {
+  #secure;
+
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#secure = url.startsWith('https');
+  }
+
+  isSecure() {
+    return this.#secure;
+  }
+
+  updateURL(url) {
+    this.url = url;
+    this.#secure = url.startsWith('https');
+  }
 }
-const s = new API("https://api.com/api/hello");
-console.log(s.isSecure());
 
-s.updateUrl("https://api.com/api/hello");
-console.log(s.isSecure());
+module.exports = API;
 
-console.log(s.url);
-module.exports={API}
